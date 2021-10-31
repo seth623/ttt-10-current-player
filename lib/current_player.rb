83567f69a9997e8e153391220c4446d6572aaee0
seth <seth623@gmail.com>
2021-10-31 15:52:02 -0600

    initial commit

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+#Turn Count
+def turn_count(board)
+counter = 0
+    board.each do |space|
+        if space != " " || "" || nil
+            counter += 1
+        end
+    end 
+return counter  
+end
+
+
+#Current Player
+def current_player(board)
+    if turn_count(board) % 2 == 0
+        return "X"
+    else 
+        return "O"
+    end
+end
+
